52af21b847aefceaa3ab6215927b40d03deadfd7
Nick <nick.naumann@mailbox.tu-dresden.de>
2026-01-22 21:05:45 +0100

    Add libk/Dynamic_bitmap
    Change-Id: Iecc9e20ec47fec0a969ae70db3edc14e9b0ffe15
    Signed-off-by: Nick <nick.naumann@mailbox.tu-dresden.de>

---
+++ src/lib/libk/dynamic_bitmap.cpp
@@ -0,0 +1,357 @@
+// vi:set ft=cpp: -*- Mode: C++ -*-
+/*
+ * Author(s): Nick Naumann <nick.naumann@mailbox.tu-dresden.de>
+ */
+
+INTERFACE:
+
+/**
+ * \file
+ *
+ * Dynamically sizable variant of the fixed-size bitmap.
+ */
+
+#include "bitmap.h"
+
+/**
+ * Dynamic bitmap implementation.
+ */
+template<typename _Alloc>
+class Dynamic_bitmap : public Bitmap_storage<unsigned long *>
+{
+public:
+  Dynamic_bitmap(size_t initial_number_of_bits, _Alloc allocator)
+  : _size(initial_number_of_bits)
+  , _internal_storage_count(Bitmap_storage::nr_elems(initial_number_of_bits))
+  , _static_storage(0)
+  , _allocator(allocator)
+  {
+    _bits = &_static_storage;
+
+    if (initial_number_of_bits > Bpl)
+      _bits = static_cast<Storage_type>(
+         _allocator.alloc(sizeof(Bitmap_elem_type) * _internal_storage_count)
+      );
+  }
+
+  virtual ~Dynamic_bitmap()
+  {
+    if (_internal_storage_count > 1)
+      _allocator.free(sizeof(Bitmap_elem_type) * _internal_storage_count,
+                      _bits);
+  }
+
+  /**
+   * Copy constructor.
+   *
+   * \param o  Dynamic_bitmap to copy from.
+   */
+  Dynamic_bitmap(Dynamic_bitmap const &o)
+  : _size(o._size)
+  , _internal_storage_count(1)
+  , _static_storage(0)
+  , _allocator(o._allocator)
+  {
+    _bits = &_static_storage;
+
+    _static_storage = o._static_storage;
+
+    reserve(o._size);
+
+    for (size_t i = 0; i < o._internal_storage_count; ++i)
+      _bits[i] = o._bits[i];
+  }
+
+  /**
+   * Extends the capacity of the bitmap ti be able to store at least the
+   * requested number of bits.
+   */
+  size_t reserve(size_t new_bit_capacity)
+  {
+    if (new_bit_capacity > capacity())
+      {
+        unsigned new_num_elements
+          = max(Bitmap_storage::nr_elems(new_bit_capacity),
+                static_cast<size_t>(_internal_storage_count * 2));
+        Storage_type expanded_storage = static_cast<Storage_type>(
+          _allocator.alloc(sizeof(Bitmap_elem_type) * new_num_elements)
+        );
+
+        for (unsigned i = 0; i < _internal_storage_count; ++i)
+          expanded_storage[i] = _bits[i];
+
+        if (_bits != &_static_storage)
+          _allocator.free(sizeof(Bitmap_elem_type) * _internal_storage_count,
+                          _bits);
+
+        _bits = expanded_storage;
+        _internal_storage_count = new_num_elements;
+      }
+
+      return capacity();
+  }
+
+  /**
+   * Number of bits that can be stored before the internal storage
+   * has to be expanded to store more.
+   */
+  size_t capacity() const
+  {
+    return Bpl * _internal_storage_count;
+  }
+
+  /**
+   * Get the number of meaningful bits in the bitmap.
+   */
+  size_t size() const
+  { return _size; }
+
+  /**
+   * Invert all bits.
+   */
+  void invert()
+  {
+    for (unsigned i = 0; i < _internal_storage_count; ++i)
+        _bits[i] = ~_bits[i];
+  }
+
+  /**
+   * Clear all bits in the bitmap.
+   *
+   * This is an optimized version for the single scalar unsigned long storage
+   * type.
+   */
+  void clear_all()
+  {
+    for (unsigned i = 0; i < _internal_storage_count; ++i)
+        _bits[i] = 0;
+  }
+
+  /**
+   * Get 1 plus the index of the least significant set bit in the bitmap
+   * ("find first set"), starting at the given bit index.
+   *
+   * \param bit  Bit index to start search at (inclusive).
+   *
+   * \retval 0 if the bitmap (starting from the given bit index) contains only
+   *         cleared bits.
+   * \return 1 plus the index of the least significant set bit in the bitmap
+   *         (starting at the given bit index).
+   */
+  size_t ffs(size_t bit) const
+  {
+    size_t idx = bit / Bpl;
+    size_t pos = bit % Bpl;
+
+    for (size_t i = idx; i < _internal_storage_count; ++i)
+      {
+        Bitmap_elem_type elem = _bits[i];
+        elem >>= pos;
+
+        unsigned int r = __builtin_ffsl(elem);
+        if (r > 0)
+          return r + (i * Bpl) + pos;
+
+        pos = 0;
+      }
+
+    return 0;
+  }
+
+  /**
+   * Get the number of bits set in this bitmap.
+   *
+   * \retval Number of bits set in this bitmap.
+   */
+  unsigned popcount() const
+  {
+    unsigned count = 0;
+    for (unsigned i = 0; i < _internal_storage_count; ++i)
+      count += __builtin_popcountl(_bits[i]);
+    return count;
+  }
+
+  /**
+   * Sets the first n bits starting from the least significant bit.
+   *
+   * \param n  Number of bits that should be set.
+   */
+  void set_first_bits(unsigned n)
+  {
+    unsigned bits_left_to_set = n;
+    for (unsigned i = 0; i < _internal_storage_count; ++i)
+      {
+        Bitmap_elem_type elem = 0;
+        if (bits_left_to_set >= Bpl)
+          _bits[i] = ~elem;
+        else
+          {
+            _bits[i] = ~elem >> (Bpl - bits_left_to_set);
+            break;
+          }
+        bits_left_to_set -= Bpl;
+      }
+  }
+
+  /**
+   * Provide raw access to underlying storage.
+   */
+  unsigned long const *raw() const
+  {
+    if (_internal_storage_count == 1)
+      return &_static_storage;
+
+    return this->_bits;
+  }
+
+  /**
+   * Check whether all bits of the bitmap are cleared.
+   *
+   * \retval False if at least one bit in the bitmap is set.
+   * \retval True if all bits in the bitmap are cleared.
+   */
+  bool is_empty() const
+  {
+    for (size_t i = 0; i < _internal_storage_count; ++i)
+      if (this->_bits[i])
+        return false;
+
+    return true;
+  }
+
+  /**
+   * Assignment operator.
+   *
+   * This variant accepts the source bitmap of the same type.
+   * If the source bitmap is larger, the capacity of this bitmap is extended
+   * before performing the operation.
+   *
+   * \param o  Dynamic_bitmap to assign from.
+   */
+  Dynamic_bitmap &operator =(Dynamic_bitmap const &o)
+  {
+    grow_to_fit(o);
+
+    for (size_t i = 0; i < o._internal_storage_count; ++i)
+      _bits[i] = o._bits[i];
+
+    return *this;
+  }
+
+  /** Disjoint operator.
+   *
+   * Logically disjoint (or) the bitmap with a different bitmap.
+   * If the source bitmap is larger, the capacity of this bitmap is extended
+   * before performing the operation.
+   *
+   * \param o  Bitmap to logically disjoint the bitmap with.
+   */
+  Dynamic_bitmap &operator |=(Dynamic_bitmap const &o)
+  {
+    grow_to_fit(o);
+
+    for (size_t i = 0; i < o._internal_storage_count; ++i)
+      _bits[i] |= o._bits[i];
+
+    return *this;
+  }
+
+  /** Or operator.
+   */
+  Dynamic_bitmap operator |(Dynamic_bitmap const &o) const
+  {
+    Dynamic_bitmap m(*this);
+    m |= o;
+    return m;
+  }
+
+  /** Conjoint operator.
+   *
+   * Logically conjoint (and) the bitmap with a different bitmap.
+   * If the source bitmap is larger, the capacity of this bitmap is extended
+   * before performing the operation.
+   *
+   * \param o  Bitmap to logically conjoint the bitmap with.
+   */
+  Dynamic_bitmap &operator &=(Dynamic_bitmap const &o)
+  {
+    grow_to_fit(o);
+
+    for (size_t i = 0; i < o._internal_storage_count; ++i)
+      _bits[i] &= o._bits[i];
+
+    for (size_t i = o._internal_storage_count; i < _internal_storage_count; ++i)
+      _bits[i] = 0;
+
+    return *this;
+  }
+
+  /** And operator.
+   */
+  Dynamic_bitmap operator &(Dynamic_bitmap const &o) const
+  {
+    Dynamic_bitmap m(*this);
+    m &= o;
+    return m;
+  }
+
+  /** Not operator.
+   *
+   * Logically invert (not) the bitmap.
+   */
+  Dynamic_bitmap operator ~() const
+  {
+    Dynamic_bitmap m(*this);
+    m.invert();
+    return m;
+  }
+
+  /**
+   * Equal compare the bitmap with a different bitmap.
+   *
+   * The semantics of this operator is the same as the conjunction of the
+   * results of the == operator applied on each pair of corresponding bits of
+   * the bitmaps separately.
+   *
+   * \param o  Bitmap to compare the bitmap with.
+   *
+   * \retval true   All bits in the common range of the bitmaps match and all
+   *                bits beyond the common range are unset.
+   * \retval false  Some bits in the common range of the bitmaps do not match
+   *                or some bits beyond the common range are set.
+   */
+  bool operator ==(Dynamic_bitmap const &o) const
+  {
+    // Check the common range of bits.
+    for (size_t i = 0; i < min(_size, o._size); ++i)
+      if (this->operator[](i) != o[i])
+        return false;
+
+    // Check the bits in this bitmap beyond the common range.
+    for (size_t i = o._size; i < _size; ++i)
+      if (this->operator[](i))
+        return false;
+
+    // Check the bits in the other bitmap beyond the common range.
+    for (size_t i = _size; i < o._size; ++i)
+      if (o[i])
+        return false;
+
+    return true;
+  }
+
+private:
+  void grow_to_fit(Dynamic_bitmap const &o)
+  {
+    if (o._size > _size)
+      _size = o._size;
+
+    if (o._size > capacity())
+      _size = reserve(o._size);
+  }
+
+  size_t _size;
+  unsigned _internal_storage_count;
+  Bitmap_elem_type _static_storage;
+  _Alloc _allocator;
+};
