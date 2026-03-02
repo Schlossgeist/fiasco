ed287427ba6c81f1f90cf59d424e0224e71c2768
Nick <nick.naumann@mailbox.tu-dresden.de>
2026-03-02 23:25:51 +0100

    libc: add C++26-style assert macros
    Change-Id: I9dcd0f198e1853c6fae05fc6854f7930f0a1f078
    Signed-off-by: Nick <nick.naumann@mailbox.tu-dresden.de>

--- src/kern/assertion.cpp
+++ src/kern/assertion.cpp
@@ -4,6 +4,8 @@ IMPLEMENTATION:
 #include <cstdio>
 #include <stdlib.h>
 
+#include "ansi.h"
+
 #include "kernel_console.h"
 #include "thread.h"
 
@@ -23,3 +25,21 @@ assert_fail(char const *expr_msg, char const *file, unsigned int line,
   Thread::system_abort();
 }
 
+extern "C"
+void
+contract_assert_fail(char const *type_str, char const *expr_msg,
+                     char const *file, unsigned int line, char const *func)
+{
+  // Make sure that GZIP mode is off.
+  //
+  // We need to use the console_unchecked() method here to avoid potential
+  // infinite recursion (calling assert() in the regular console() method).
+  Kconsole::console_unchecked()->end_exclusive(Console::GZIP);
+
+  printf("\n"          ANSI("%s", RED, BOLD)
+         " in "        ANSI("%s", BOLD)
+         " at %s:%u: " ANSI("%s", CYAN, BOLD) "\n",
+         type_str, func, file, line, expr_msg);
+
+  Thread::system_abort();
+}
--- src/lib/libc/include/assert.h
+++ src/lib/libc/include/assert.h
@@ -10,18 +10,37 @@ assert_fail(char const *expr_msg, char const *file, unsigned int line,
             void const *caller)
      __attribute__ ((__noreturn__));
 
+void
+contract_assert_fail(char const *type_str, char const *expr_msg,
+					 char const *file, unsigned int line, char const *func)
+	 __attribute__ ((__noreturn__));
+
 __END_DECLS
 
 #define ASSERT_EXPECT_FALSE(exp)  __builtin_expect((exp), 0)
 
 #ifdef NDEBUG
-#define assert(expr) do {} while (0)
-#define check(expr) (void)(expr)
+#define assert(expr)		do {} while (0)
+#define precondition(expr)	do {} while (0)
+#define invariant(expr)		do {} while (0)
+#define postcondition(expr)	do {} while (0)
+#define check(expr)			(void)(expr)
 #else
-# define assert(expr)						\
-  ((void)((ASSERT_EXPECT_FALSE(!(expr)))			\
-	  ? (assert_fail(#expr, __FILE__, __LINE__,             \
-                         __builtin_return_address(0)), 0)	\
+# define assert(expr)										\
+  ((void)((ASSERT_EXPECT_FALSE(!(expr)))					\
+	  ? (assert_fail(#expr, __FILE__, __LINE__,         	\
+                     __builtin_return_address(0)), 0)		\
 	  : 0))
+
+# define contract_assert(msg, expr)								\
+  ((void)((ASSERT_EXPECT_FALSE(!(expr)))						\
+	  ? (contract_assert_fail(msg, #expr, __FILE__, __LINE__,	\
+							  __FUNCTION__), 0)					\
+	  : 0))
+
+#define precondition(expr)		contract_assert("Precondition failed", expr)
+#define invariant(expr)		    contract_assert("Invariant failed", expr)
+#define postcondition(expr)		contract_assert("Postcondition failed", expr)
+
 # define check(expr) assert(expr)
 #endif
