716c6794c0b3b05ea93142eeb2930b3000633c4c
Nick <nick.naumann@mailbox.tu-dresden.de>
2026-03-10 13:49:15 +0100

    libc: implement rand() (part 2)
    Change-Id: I2cdf1aa02f39f8598a494c83ef9ebcd872a13266
    Signed-off-by: Nick <nick.naumann@mailbox.tu-dresden.de>

--- src/lib/libc/src/stdlib/rand.c
+++ src/lib/libc/src/stdlib/rand.c
@@ -1,15 +1,13 @@
 #include <stdlib.h>
 
+#include "entropy.h"
+
 static int random_int = 0; // = 0xF1A5C0;
 
 int rand(void)
 {
-    if (random_int == 0) {
-        unsigned lo, hi;
-        asm volatile("mrrc p15, 0, %0, %1, c14"
-                     : "=r"(lo), "=r"(hi));
-        random_int = lo;
-    }
+    if (random_int == 0)
+        random_int = __entropy();
 
     random_int ^= random_int << 13;
     random_int ^= random_int >> 17;
